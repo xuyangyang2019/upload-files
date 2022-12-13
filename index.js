a29e1abb282a66a1a1d9890d7b6d9e9c2d092e5d
xuyangyang2019 <15268296911@163.com>
2022-12-13 12:51:40 +0800

    路由中间件

--- index.js
+++ index.js
@@ -1,6 +1,55 @@
 const Koa = require('koa')
 const path = require('path')
-const loggerAsync  = require('./middleware/logger-async')
+const loggerAsync = require('./middleware/logger-async')
+const Router = require('koa-router')
+
+const fs = require('fs')
+/**
+ * 用Promise封装异步读取文件方法
+ * @param  {string} page html文件名称
+ * @return {promise}
+ */
+function render(page) {
+  return new Promise((resolve, reject) => {
+    let viewUrl = `./views/${page}`
+    fs.readFile(viewUrl, 'binary', (err, data) => {
+      if (err) {
+        reject(err)
+      } else {
+        resolve(data)
+      }
+    })
+  })
+}
+
+let home = new Router()
+// 子路由1
+home.get('/', async (ctx) => {
+  let html = `
+      <ul>
+        <li><a href="/page/helloworld">/page/helloworld</a></li>
+        <li><a href="/page/404">/page/404</a></li>
+      </ul>
+    `
+  //   ctx.body = html
+  ctx.body = await render('index.html')
+})
+// 子路由2
+let page = new Router()
+page
+  .get('/404', async (ctx) => {
+    ctx.body = '404 page!'
+    // ctx.body = await render('404.html')
+  })
+  .get('/todo', async (ctx) => {
+    // ctx.body = 'helloworld page!'
+    ctx.body = await render('todo.html')
+  })
+
+// 装载所有子路由
+let router = new Router()
+router.use('/', home.routes(), home.allowedMethods())
+router.use('/page', page.routes(), page.allowedMethods())
 
 // const { uploadFile } = require('./util/upload')
 // const views = require('koa-views')
@@ -13,7 +62,10 @@ const app = new Koa()
 //   },
 // })
 
+// 加载日志中间件
 app.use(loggerAsync())
+// 加载路由中间件
+app.use(router.routes()).use(router.allowedMethods())
 
 /**
  * 使用第三方中间件 start
@@ -34,28 +86,28 @@ app.use(loggerAsync())
  */
 
 app.use(async (ctx) => {
-  ctx.body = 'hello wlorld';
+  ctx.body = 'hello wlorld'
 
-//   if (ctx.method === 'GET') {
-//     let title = 'upload pic async'
-//     await ctx.render('index', {
-//       title,
-//     })
-//   } else if (ctx.url === '/api/picture/upload.json' && ctx.method === 'POST') {
-//     // 上传文件请求处理
-//     let result = { success: false }
-//     let serverFilePath = path.join(__dirname, 'static/image')
+  //   if (ctx.method === 'GET') {
+  //     let title = 'upload pic async'
+  //     await ctx.render('index', {
+  //       title,
+  //     })
+  //   } else if (ctx.url === '/api/picture/upload.json' && ctx.method === 'POST') {
+  //     // 上传文件请求处理
+  //     let result = { success: false }
+  //     let serverFilePath = path.join(__dirname, 'static/image')
 
-//     // 上传文件事件
-//     result = await uploadFile(ctx, {
-//       fileType: 'album',
-//       path: serverFilePath,
-//     })
-//     ctx.body = result
-//   } else {
-//     // 其他请求显示404
-//     ctx.body = '<h1>404！！！ o(╯□╰)o</h1>'
-//   }
+  //     // 上传文件事件
+  //     result = await uploadFile(ctx, {
+  //       fileType: 'album',
+  //       path: serverFilePath,
+  //     })
+  //     ctx.body = result
+  //   } else {
+  //     // 其他请求显示404
+  //     ctx.body = '<h1>404！！！ o(╯□╰)o</h1>'
+  //   }
 })
 
 app.listen(3000, () => {
