c4e3dad5b1661d6243a5def0eb0bab95c33e6699
xuyangyang2019 <15268296911@163.com>
2022-12-13 12:26:20 +0800

    自定义日志中间件

--- index.js
+++ index.js
@@ -1,42 +1,63 @@
 const Koa = require('koa')
 const path = require('path')
-const app = new Koa()
+const loggerAsync  = require('./middleware/logger-async')
 
-const { uploadFile } = require('./util/upload')
+// const { uploadFile } = require('./util/upload')
+// const views = require('koa-views')
+// const static = require('koa-static')
+
+const app = new Koa()
+// const render = views(__dirname + '/views', {
+//   map: {
+//     html: 'ejs',
+//   },
+// })
+
+app.use(loggerAsync())
+
+/**
+ * 使用第三方中间件 start
+ */
+// app.use(
+//   views(path.join(__dirname, './view'), {
+//     extension: 'ejs',
+//   })
+// )
+// app.use(render)
+
+// 静态资源目录对于相对入口文件index.js的路径
+// const staticPath = './static'
+
+// app.use(static(path.join(__dirname, staticPath)))
+/**
+ * 使用第三方中间件 end
+ */
 
 app.use(async (ctx) => {
-  if (ctx.url === '/' && ctx.method === 'GET') {
-    // 当GET请求时候返回表单页面
-    let html = `
-      <h1>koa2 upload demo</h1>
-      <form method="POST" action="/upload.json" enctype="multipart/form-data">
-        <p>file upload</p>
-        <span>picName:</span><input name="picName" type="text" /><br/>
-        <input name="file" type="file" /><br/><br/>
-        <button type="submit">submit</button>
-      </form>
-    `
-    ctx.body = html
-  } else if (ctx.url === '/upload.json' && ctx.method === 'POST') {
-    // 上传文件请求处理
-    let result = { success: false }
-    let serverFilePath = path.join(__dirname, 'upload-files')
-    console.log(serverFilePath)
-    let nd = new Date()
-    // 上传文件事件
-    result = await uploadFile(ctx, {
-      //   fileType: 'album',
-      fileType: nd.getFullYear() + '-' + (nd.getMonth() + 1) + '-' + nd.getDate(),
-      path: serverFilePath,
-    })
+  ctx.body = 'hello wlorld';
 
-    ctx.body = result
-  } else {
-    // 其他请求显示404
-    ctx.body = '<h1>404！！！ o(╯□╰)o</h1>'
-  }
+//   if (ctx.method === 'GET') {
+//     let title = 'upload pic async'
+//     await ctx.render('index', {
+//       title,
+//     })
+//   } else if (ctx.url === '/api/picture/upload.json' && ctx.method === 'POST') {
+//     // 上传文件请求处理
+//     let result = { success: false }
+//     let serverFilePath = path.join(__dirname, 'static/image')
+
+//     // 上传文件事件
+//     result = await uploadFile(ctx, {
+//       fileType: 'album',
+//       path: serverFilePath,
+//     })
+//     ctx.body = result
+//   } else {
+//     // 其他请求显示404
+//     ctx.body = '<h1>404！！！ o(╯□╰)o</h1>'
+//   }
 })
 
 app.listen(3000, () => {
-  console.log('[demo] upload-simple is starting at port 3000')
+  console.log('[demo] upload-pic-async is starting at port 3000')
 })
