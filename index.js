91fad05d9f4ff590f186832908409af46de93c49
xuyangyang2019 <15268296911@163.com>
2022-12-14 16:35:08 +0800

    静态资源加载

--- index.js
+++ index.js
@@ -7,6 +7,18 @@ const loggerAsync = require('./middleware/logger-async')
 const Router = require('koa-router')
 const bodyParser = require('koa-bodyparser')
 
+const content = require('./util/content')
+const mimes = require('./util/mimes')
+
+// 静态资源目录对于相对入口文件index.js的路径
+const staticPath = './static'
+// 解析资源类型
+function parseMime(url) {
+  let extName = path.extname(url)
+  extName = extName ? extName.slice(1) : 'unknown'
+  return mimes[extName]
+}
+
 /**
  * 用Promise封装异步读取文件方法
  * @param  {string} page html文件名称
@@ -28,24 +40,15 @@ function render(page) {
 let home = new Router()
 // 子路由1
 home.get('/', async (ctx) => {
-  let html = `
-      <ul>
-        <li><a href="/page/helloworld">/page/helloworld</a></li>
-        <li><a href="/page/404">/page/404</a></li>
-      </ul>
-    `
-  //   ctx.body = html
   ctx.body = await render('index.html')
 })
 // 子路由2
 let page = new Router()
 page
   .get('/404', async (ctx) => {
-    ctx.body = '404 page!'
-    // ctx.body = await render('404.html')
+    ctx.body = await render('404.html')
   })
   .get('/todo', async (ctx) => {
-    // ctx.body = 'helloworld page!'
     ctx.body = await render('todo.html')
   })
 
@@ -92,13 +95,36 @@ app.use(router.routes()).use(router.allowedMethods())
  */
 
 app.use(async (ctx) => {
-  console.log(ctx.url)
-  console.log(ctx.method)
+  // 解析post body
   if (ctx.url === '/' && ctx.method === 'POST') {
     // 当POST请求的时候，解析POST表单里的数据，并显示出来
     let postData = ctx.request.body
     ctx.body = postData
   }
+
+  if (ctx.url.startsWith('/static') && ctx.method === 'GET') {
+    // 静态资源目录在本地的绝对路径
+    let fullStaticPath = path.join(__dirname, staticPath)
+    // 获取静态资源内容，有可能是文件内容，目录，或404
+    let _content = await content(ctx, fullStaticPath)
+    // 解析请求内容的类型
+    let _mime = parseMime(ctx.url)
+    // 如果有对应的文件类型，就配置上下文的类型
+    if (_mime) {
+      ctx.type = _mime
+    }
+    // 输出静态资源内容
+    if (_mime && _mime.indexOf('image/') >= 0) {
+      // 如果是图片，则用node原生res，输出二进制数据
+      ctx.res.writeHead(200)
+      ctx.res.write(_content, 'binary')
+      ctx.res.end()
+    } else {
+      // 其他则输出文本
+      ctx.body = _content
+    }
+  }
+
   //   ctx.body = 'hello wlorld'
   //   //   if (ctx.method === 'GET') {
   //   //     let title = 'upload pic async'
@@ -122,5 +148,6 @@ app.use(async (ctx) => {
 })
 
 app.listen(3000, () => {
-  console.log('[demo] upload-pic-async is starting at port 3000')
+  // console.log('[demo] upload-pic-async is starting at port 3000')
+  console.log('upload-files is starting at: http://localhost:3000')
 })
--- util/content.js
+++ util/content.js
@@ -15,7 +15,7 @@ const file = require('./file')
  */
 async function content(ctx, fullStaticPath) {
   // 封装请求资源的完绝对径
-  let reqPath = path.join(fullStaticPath, ctx.url)
+  let reqPath = path.join(fullStaticPath, ctx.url.replace('/static', '/'))
 
   // 判断请求路径是否为存在目录或者文件
   let exist = fs.existsSync(reqPath)
@@ -24,14 +24,14 @@ async function content(ctx, fullStaticPath) {
   let content = ''
 
   if (!exist) {
-    //如果请求路径不存在，返回404
+    // 如果请求路径不存在，返回404
     content = '404 Not Found! o(╯□╰)o！'
   } else {
-    //判断访问地址是文件夹还是文件
+    // 判断访问地址是文件夹还是文件
     let stat = fs.statSync(reqPath)
 
     if (stat.isDirectory()) {
-      //如果为目录，则渲读取目录内容
+      // 如果为目录，则渲读取目录内容
       content = dir(ctx.url, reqPath)
     } else {
       // 如果请求为文件，则读取文件内容
