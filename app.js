836535d381f0b861ae3ba87ebb91648d6e2b16c0
xuyangyang2019 <15268296911@163.com>
2023-01-05 17:50:42 +0800

    处理跨域问题

--- app.js
+++ app.js
@@ -99,26 +99,26 @@ app.use(static(__dirname + '/static'), {
 
 // 处理跨域
 // 全部允许跨域
-app.use(cors())
+// app.use(cors())
 // 设置多个域名可跨域
-// app.use(
-//   cors({
-//     origin: function (ctx) {
-//       // 设置允许来自指定域名请求
-//       const whiteList = ['http://xyy.life', 'http://localhost:9527'] // 可跨域白名单
-//       const url = ctx.header.referer ? ctx.header.referer.substr(0, ctx.header.referer.length - 1) : ''
-//       if (whiteList.includes(url)) {
-//         return url // 注意，这里域名末尾不能带/，否则不成功，所以在之前我把/通过substr干掉了
-//       }
-//       return 'http://localhost::3000' // 默认允许本地请求3000端口可跨域
-//     },
-//     maxAge: 5, // 指定本次预检请求的有效期，单位为秒。
-//     credentials: true, // 是否允许发送Cookie
-//     allowMethods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // 设置所允许的HTTP请求方法
-//     allowHeaders: ['Content-Type', 'Authorization', 'Accept'], // 设置服务器支持的所有头信息字段
-//     exposeHeaders: ['WWW-Authenticate', 'Server-Authorization'] // 设置获取其他自定义字段
-//   })
-// )
+app.use(
+  cors({
+    origin: function (ctx) {
+      // 设置允许来自指定域名请求
+      const whiteList = ['http://xyy.life', 'http://localhost:3001'] // 可跨域白名单
+      const url = ctx.header.referer ? ctx.header.referer.substr(0, ctx.header.referer.length - 1) : ''
+      if (whiteList.includes(url)) {
+        return url // 注意，这里域名末尾不能带/，否则不成功，所以在之前我把/通过substr干掉了
+      }
+      return 'http://localhost::3001' // 默认允许本地请求3000端口可跨域
+    },
+    maxAge: 5, // 指定本次预检请求的有效期，单位为秒。
+    credentials: true, // 是否允许发送Cookie
+    allowMethods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // 设置所允许的HTTP请求方法
+    allowHeaders: ['Content-Type', 'Authorization', 'Accept'], // 设置服务器支持的所有头信息字段
+    exposeHeaders: ['WWW-Authenticate', 'Server-Authorization'] // 设置获取其他自定义字段
+  })
+)
 
 app.use(async (ctx) => {
   // // 解析post body
