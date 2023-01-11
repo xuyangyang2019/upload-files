148701bf1b818e610b501f5e02e414558a2dd61e
xuyangyang2019 <15268296911@163.com>
2023-01-11 18:03:46 +0800

    错误信息写入到log文件中

--- middlewares/error.js
+++ middlewares/error.js
@@ -1,10 +1,12 @@
 // 这个middleware处理在其它middleware中出现的异常
+const log4j = require('../utils/log4j') // 日志工具
+
 // 并将异常消息回传给客户端：{ code: '错误代码', msg: '错误信息' }
 const errorHandler = async (ctx, next) => {
   try {
     await next()
   } catch (err) {
-    console.log('error:', err)
+    log4j.error(err) // 错误写入日志文件中
 
     const { status, name, message, code } = err
     // 保证返回状态是 200, 这样前端不会抛出异常
@@ -12,7 +14,7 @@ const errorHandler = async (ctx, next) => {
     ctx.body = {
       code: code,
       name: name,
-      msg: message
+      msg: message,
     }
 
     // 手动释放error事件
