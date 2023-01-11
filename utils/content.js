bc2200241a0645ac0fcd2ed974f3e40049a84a09
xuyangyang2019 <15268296911@163.com>
2023-01-11 11:37:43 +0800

    解析log和text类型的文件

--- utils/content.js
+++ utils/content.js
@@ -4,18 +4,16 @@ const fs = require('fs')
 // 封装读取目录内容方法
 const dir = require('./dir')
 
-// 封装读取文件内容方法
-const file = require('./file')
-
 /**
  * 获取静态资源内容
  * @param  {object} ctx koa上下文
  * @param  {string} 静态资源目录在本地的绝对路径
  * @return  {string} 请求获取到的本地内容
  */
-async function content(ctx, fullStaticPath) {
+async function content(ctx, fullStaticPath, mime) {
   // 封装请求资源的完绝对径
   let reqPath = path.join(fullStaticPath, ctx.url)
+  console.log(reqPath)
 
   // 判断请求路径是否为存在目录或者文件
   let exist = fs.existsSync(reqPath)
@@ -34,8 +32,17 @@ async function content(ctx, fullStaticPath) {
       // 如果为目录，则渲读取目录内容
       content = dir(ctx.url, reqPath)
     } else {
-      // 如果请求为文件，则读取文件内容
-      content = await file(reqPath)
+      // "utf8" | "binary" | "ascii" | "utf-8" | "utf16le" | "ucs2" | "ucs-2" | "base64" | "latin1" | "hex")
+      let encoding = 'utf8' // 默认
+      if (mime) {
+        if (mime.startsWith('image/')) {
+          encoding = 'binary'
+        } else if (mime.startsWith('application/')) {
+          encoding = 'ascii'
+        }
+      }
+      console.log('cotent encoding', encoding)
+      content = fs.readFileSync(reqPath, encoding)
     }
   }
 
